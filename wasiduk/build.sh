cef9ad4e58efce1d47f1355060c1fc1caeef297d
Saúl Ibarra Corretgé <s@saghul.net>
2019-07-12 22:42:15 +0200

    build: download WASI SDK automatically

--- wasiduk/build.sh
+++ wasiduk/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ ! -d wasi-sdk-5.0 ]]; then
+    curl -L -O https://github.com/CraneStation/wasi-sdk/releases/download/wasi-sdk-5/wasi-sdk-5.0-macos.tar.gz
+    tar zxvf wasi-sdk-5.0-macos.tar.gz
+fi
+
 mkdir -p build
 cd build
 cmake -DCMAKE_TOOLCHAIN_FILE=../wasi-sdk.cmake ..
