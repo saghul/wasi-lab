e1acaae181d101caae31e097b2b0d46e703a9645
Saúl Ibarra Corretgé <s@saghul.net>
2019-07-12 23:20:37 +0200

    duk: add examples

---
+++ wasiduk/examples/hello_world.js
@@ -0,0 +1,13 @@
+
+function fib(n) {
+    if (n <= 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+        return fib(n - 1) + fib(n - 2);
+}
+
+console.log("Hello World");
+console.log("fib(10)=", fib(10));
+
